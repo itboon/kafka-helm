1bea6d6b63beb2c11f671bf76d0c1d3f65999cfb
itboon <s9983@outlook.com>
2024-10-01 23:24:05 +0800

    chart: relase 17.0.2

--- scripts/gen-charts-ha.sh
+++ scripts/gen-charts-ha.sh
@@ -6,11 +6,10 @@ chart_ha_root="../charts/kafka-ha"
 rm -rf "$chart_ha_root"
 cp -a "${charts_home}/kafka" "$chart_ha_root"
 
-set -i 's/^name: kafka$/name: kafka-ha/'  "${chart_ha_root}/Chart.yaml"
-
 init_ha_values() {
   yq -n 'load("'${charts_home}/kafka/values.yaml'") * load("values-ha.yaml")' \
     | tee "${chart_ha_root}/values.yaml"
 }
 
 init_ha_values
+sed -i 's/^name: kafka$/name: kafka-ha/'  "${chart_ha_root}/Chart.yaml"
